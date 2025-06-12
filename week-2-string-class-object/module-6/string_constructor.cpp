f47b0f1d8d9dd5f6bc66d28e4b7f8c57751f03de
NS-Sheam <123sheamfeni@gmail.com>
2025-06-12 19:06:34 +0600

    add: string constructor

---
+++ week-2-string-class-object/module-6/string_constructor.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // string s = "Hello";
+    // string s("Hello World", 3);
+    // string s = "Hello World";
+    string s(26, 'A'); // Creates a string of 26 'A's
+    string t(s, 3);
+    cout << t << endl; // Output: Hello
+    return 0;
+}
